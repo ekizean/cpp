37968ec06b15d5f9e98bb523993df47a9220e5b2
Simon Ekizean <simon.ekizean@clarmeont.se>
2018-10-29 19:41:34 +0100

    state upp

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Projectstatus from "./Projectstatus";
+
 class Project extends Component {
   render() {
     return (
       <div className="Project">
-        <h1>Rubrik</h1>
-        <Projectstatus />
-        <p>Beskrivning</p>
+        <h1>{this.props.title}</h1>
+        <Projectstatus status={this.props.status} />
+        <p>{this.props.description}</p>
         <p>Radera</p>
       </div>
     );
--- src/components/Projectlist.js
+++ src/components/Projectlist.js
@@ -2,11 +2,34 @@ import React, { Component } from "react";
 import Project from "./Project";
 
 class Projectlist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectArray: [
+        {
+          title: "SAPSA",
+          description: "VR is King",
+          status: "Ej påbörjad"
+        },
+        {
+          title: "DataTjej",
+          description: "Jobbportal",
+          status: "Påbörjad"
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="Projectlist">
-        <Project />
-        <Project />
+        {this.state.projectArray.map(p => (
+          <Project
+            title={p.title}
+            description={p.description}
+            status={p.status}
+          />
+        ))}
       </div>
     );
   }
