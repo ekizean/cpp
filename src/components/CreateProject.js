213df882b2b474f55b84e1be1b273d812920008e
Simon Ekizean <simon.ekizean@clarmeont.se>
2018-11-12 17:00:39 +0100

    deleteknapp påbörjad

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -1,38 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CreateProject extends Component {
-    handleChange = this.handleChange.bind(this);
-    handleSubmit = this.handleSubmit.bind(this);
+  handleChange = this.handleChange.bind(this);
+  handleSubmit = this.handleSubmit.bind(this);
 
-    state = { 
-        title: "",
-        description: "",
-        status: "Ej påbörjad"
-    }
+  state = {
+    title: "",
+    description: "",
+    status: "Ej påbörjad"
+  };
 
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-    handleSubmit(e) {
-        e.preventDefault()
-        this.props.createProject(this.state)
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createProject(this.state);
+    this.setState({
+      title: "",
+      description: "",
+      status: "Ej påbörjad"
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                Create project stuff
-                <form onSubmit={this.handleSubmit}>
-                    <input name="title" type="text" placeholder="Titel" onChange={this.handleChange}/>
-                    <input name="description" type="text" placeholder="Beskrivning" onChange={this.handleChange}/>
-                    <input type="submit" />
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        Create project stuff
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="title"
+            type="text"
+            placeholder="Titel"
+            onChange={this.handleChange}
+            value={this.state.title}
+          />
+          <input
+            name="description"
+            type="text"
+            placeholder="Beskrivning"
+            onChange={this.handleChange}
+            value={this.state.description}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreateProject;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import Projectstatus from "./Projectstatus";
 
-const Project = (props) => {
-    return (
-      <div className="Project">
-        <h1>{props.title}</h1>
-        <Projectstatus status={props.status} changeProjectStatus={props.changeProjectStatus} id={props.id}/>
-        <p>{props.description}</p>
-        <p>Radera</p>
-      </div>
-    );
-}
+const Project = props => {
+  return (
+    <div className="Project">
+      <h1>{props.title}</h1>
+      <Projectstatus
+        status={props.status}
+        changeProjectStatus={props.changeProjectStatus}
+        id={props.id}
+      />
+      <p>{props.description}</p>
+      <button onClick={this.props.deleteProject}>Radera</button>
+    </div>
+  );
+};
 
 export default Project;
