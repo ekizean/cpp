c745039baeb049a140337bd53d1284b6d38986ae
Simon Ekizean <simon.ekizean@clarmeont.se>
2018-12-12 16:25:08 +0100

    Modal och Id-bugg fixad

--- src/App.js
+++ src/App.js
@@ -6,90 +6,104 @@ import CreateProject from "./components/CreateProject";
 import Database from "./firebase/firebase";
 import { finished } from "stream";
 import Footer from "./components/Footer";
+import Modal from "react-modal";
 
 class App extends Component {
   changeProjectStatus = this.changeProjectStatus.bind(this);
   createProject = this.createProject.bind(this);
   deleteProject = this.deleteProject.bind(this);
+  showOverview = this.showOverview.bind(this);
 
   state = {
-    projectArray: []
+    projectArray: [],
+    showOverview: false
   };
 
   changeProjectStatus(id, status) {
     let statusText = status === "Pågående" ? "Ej påbörjad" : "Pågående";
 
-    Database
-      .ref(`projects/${id}`)
-      .update({ status: statusText })
+    Database.ref(`projects/${id}`).update({ status: statusText });
 
-    this.setState(
-      prevState => {
-        return {
-          projectArray: prevState.projectArray.map(p => { 
-            if (p.id == id) {
-              p.status = statusText;
-            }
-            
-            return p; 
-          })
-        };
-      }
-    );
+    this.setState(prevState => {
+      return {
+        projectArray: prevState.projectArray.map(p => {
+          if (p.id == id) {
+            p.status = statusText;
+          }
+
+          return p;
+        })
+      };
+    });
+  }
+
+  showOverview() {
+    this.setState({
+      showOverview: true
+    });
   }
 
   createProject(newProject) {
-    Database
-      .ref("projects")
+    Database.ref("projects")
       .push(newProject)
+      .then(this.readProjectsFromDatabase());
+    this.setState({ showOverview: false });
+  }
+
+  deleteProject(id) {
+    Database.ref(`projects/${id}`)
+      .remove()
       .then(
         this.setState({
-          projectArray: [...this.state.projectArray, newProject]
+          projectArray: this.state.projectArray.filter(
+            project => project.id !== id
+          )
         })
       );
   }
 
-  deleteProject(id) {
-    Database.ref(`projects/${id}`).remove().then(
-      this.setState({
-        projectArray: this.state.projectArray.filter(project => project.id !== id)
-      })
-    );
+  readProjectsFromDatabase() {
+    Database.ref("projects")
+      .once("value")
+      .then(snapshot => {
+        const projectArray = [];
 
+        snapshot.forEach(snapshotChild => {
+          projectArray.push({
+            id: snapshotChild.key,
+            ...snapshotChild.val()
+          });
+        });
+
+        this.setState({
+          projectArray
+        });
+      });
   }
 
   componentDidMount() {
-    Database.ref("projects").once("value").then((snapshot) => {
-
-      const projectArray = [];
-
-      snapshot.forEach((snapshotChild) => {
-        projectArray.push({
-          id: snapshotChild.key,
-          ...snapshotChild.val()
-        });
-      });
-
-      this.setState({
-        projectArray
-      }, () => {console.log(this.state);});
-
-      
-      
-    });
+    this.readProjectsFromDatabase();
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <CreateProject createProject={this.createProject} />
+        <Header showOverview={this.showOverview} />
         <main>
+          <Modal
+            className="Modal"
+            isOpen={this.state.showOverview}
+            onRequestClose={() => this.setState({ showOverview: false })}
+          >
+            {this.state.showOverview && (
+              <CreateProject createProject={this.createProject} />
+            )}
+          </Modal>
           <ProjectList
             projectArray={this.state.projectArray}
             changeProjectStatus={this.changeProjectStatus}
             deleteProject={this.deleteProject}
-            />
+          />
         </main>
         <Footer />
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <div className="page-container">
-                    <h1>Projektportalen</h1>
-                    <form>
-                        <div className="searchContainer">
-                            <input className="searchInput" type="text" placeholder="Sök här..." autoFocus/>
-                            <input className="searchButton" value="Sök" type="submit"/>
-                        </div>
-                    </form>
-                    <button className="addButton">+</button>
-                </div>
+  render() {
+    return (
+      <div className="header">
+        <div className="page-container">
+          <h1>Projektportalen</h1>
+          <form>
+            <div className="searchContainer">
+              <input
+                className="searchInput"
+                type="text"
+                placeholder="Sök här..."
+                autoFocus
+              />
+              <input className="searchButton" value="Sök" type="submit" />
             </div>
-        );
-    }
+          </form>
+          <button className="addButton" onClick={this.props.showOverview}>
+            +
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
